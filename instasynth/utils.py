caa26f70d72ab9bee427368dc0b1c42424292f32
thalesbertaglia <contact@thalesbertaglia.com>
2023-08-23 10:21:28 +0200

    Improve json fix method

--- instasynth/utils.py
+++ instasynth/utils.py
@@ -40,16 +40,34 @@ def process_response_content(content: str) -> pd.DataFrame:
         )
     except Exception as e:
         logger.error(f"Failed to parse the JSON response: {e}")
+        raise e
 
 
 def format_json_string(json_str: str) -> str:
     """Format a JSON string to correct common mistakes."""
     # Simplify whitespace, but preserve structure
     json_str = "\n".join(line.strip() for line in json_str.splitlines() if line.strip())
-    # Add missing commas between dictionary items
-    json_str = re.sub(r'(["}\]])\s*("{|"[a-zA-Z])', r"\1,\2", json_str)
-    # Remove trailing commas
-    json_str = re.sub(r",\s*([}\]])", r"\1", json_str)
+    # Replace \r\n with \n for consistency
+    json_str = json_str.replace("\r\n", "\n")
+    # Remove forward slashes before line breaks
+    json_str = json_str.replace("\\\n", "\n")
+    # Add missing commas between consecutive quotes
+    json_str = re.sub(r'("\s+)\n(")', r"\1,\n\2", json_str)
+    try:
+        _ = json.loads(json_str)
+    except json.JSONDecodeError:
+        # Combine split posts into one line
+        json_str = re.sub(r'(?<=,)\n"', ' "', json_str)
+        # Add missing commas between posts
+        json_str = re.sub(r'("\n)\s*"', r"\1,", json_str)
+        # Remove multiple line breaks (keeping a single line break)
+        json_str = re.sub(r"\n+", "\n", json_str)
+        # Remove consecutive commas
+        json_str = re.sub(r",\s*,", ",", json_str)
+        # Remove trailing commas
+        json_str = re.sub(r",\s*([}\]])", r"\1", json_str)
+
+        return json_str
 
     return json_str
 
