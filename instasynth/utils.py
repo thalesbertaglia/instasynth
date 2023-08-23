b71f7c2c6eca520950ebd4832cd6e862bc0804f8
thalesbertaglia <contact@thalesbertaglia.com>
2023-08-23 16:11:05 +0200

    Refactor fix json string

--- instasynth/utils.py
+++ instasynth/utils.py
@@ -36,40 +36,30 @@ def get_pathnames(experiment_identifier: str) -> Tuple[str, str]:
 def process_response_content(content: str) -> pd.DataFrame:
     try:
         return pd.DataFrame(
-            json.loads(format_json_string(content))["posts"], columns=["caption"]
+            json.loads(fix_json_string(content))["posts"], columns=["caption"]
         )
     except Exception as e:
         logger.error(f"Failed to parse the JSON response: {e}")
         raise e
 
 
-def format_json_string(json_str: str) -> str:
+def fix_json_string(json_str: str) -> str:
     """Format a JSON string to correct common mistakes."""
-    # Simplify whitespace, but preserve structure
-    json_str = "\n".join(line.strip() for line in json_str.splitlines() if line.strip())
-    # Replace \r\n with \n for consistency
-    json_str = json_str.replace("\r\n", "\n")
-    # Remove forward slashes before line breaks
-    json_str = json_str.replace("\\\n", "\n")
-    # Add missing commas between consecutive quotes
-    json_str = re.sub(r'("\s+)\n(")', r"\1,\n\2", json_str)
-    try:
-        _ = json.loads(json_str)
-    except json.JSONDecodeError:
-        # Combine split posts into one line
-        json_str = re.sub(r'(?<=,)\n"', ' "', json_str)
-        # Add missing commas between posts
-        json_str = re.sub(r'("\n)\s*"', r"\1,", json_str)
-        # Remove multiple line breaks (keeping a single line break)
-        json_str = re.sub(r"\n+", "\n", json_str)
-        # Remove consecutive commas
-        json_str = re.sub(r",\s*,", ",", json_str)
-        # Remove trailing commas
-        json_str = re.sub(r",\s*([}\]])", r"\1", json_str)
+    # Normalize line breaks and strip whitespace
+    normalized_lines = [
+        line.strip()
+        for line in json_str.replace("\r\n", "\n").splitlines()
+        if line.strip()
+    ]
+    normalized_str = "\n".join(normalized_lines).replace("\\\n", "\n")
+    # Ensure proper comma placements between consecutive quotes (between posts)
+    with_commas = re.sub(r'"\s*\n"', r'",\n"', normalized_str)
+    # Remove trailing commas in lists or objects
+    no_trailing_commas = re.sub(r",\s*([}\]])", r"\1", with_commas)
+    # Remove unnecessary whitespace characters
+    whitespace_cleaned = no_trailing_commas.replace("\t", " ").replace("\n", " ")
 
-        return json_str
-
-    return json_str
+    return whitespace_cleaned
 
 
 def format_examples(examples: list) -> str:
